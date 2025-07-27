8ad69da3dc1ec542694123ba36f7eb10f2309419
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2025-07-27 23:02:04 +0530

    check whethe user authenticated or not

--- controllers/authController.js
+++ controllers/authController.js
@@ -183,3 +183,15 @@ export const verifyEmail = async (req, res) => {
     }
 
 }
+
+export const isAuthenticated = async (req, res) => {
+
+    try{
+
+        return res.json({success:true})
+
+    }catch(error){
+        res.json({success:false, message:error.message})
+    }
+
+}
