dbabeac2def0fb6362a39a1bcae0ac87bd0753ce
Reshma Mohamed <mohamed-im22087@stu.kln.ac.lk>
2025-08-03 13:35:09 +0530

    update

--- server.js
+++ server.js
@@ -12,17 +12,30 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
+const allowedOrigin = 'https://cake-ly.netlify.app';
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'https://cake-ly.netlify.app',
+  origin: allowedOrigin,
   credentials: true,
 }));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
-app.get('/', (req, res) => res.send("API working"));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedOrigin);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+
+app.get('/', (req, res) => res.send("API working"));
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
