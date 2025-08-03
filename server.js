662e71f35a400ee15b105030a18bd0b71d6b41be
Reshma Mohamed <mohamed-im22087@stu.kln.ac.lk>
2025-08-03 14:33:59 +0530

    update

--- server.js
+++ server.js
@@ -3,29 +3,24 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
-import authRouter from './routes/authRoutes.js';
+import authRouter from './routes/authRoutes.js'
 import userRouter from './routes/userRoutes.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpecs from './config/swagger.js';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigin = 'https://cake-ly.netlify.app';
+const allowedOrigins = ['http://localhost:3000'];
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use(cors({
-  origin: allowedOrigin,
-  credentials: true,
-}));
-
+app.use(cors({origin: allowedOrigins, credentials: true}));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
-app.get('/', (req, res) => res.send("API working"));
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
+app.get('/', (req, res) => res.send("API working"))
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
